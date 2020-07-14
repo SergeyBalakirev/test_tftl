c5f733b346d6ce369eb52fe9ad8fe67374dd12d7
SergeyBalakirev <balakirev.sergey87@gmail.com>
2020-07-14 21:47:32 +0300

    added swiper and footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,11 @@
-$(function(){
+$(function () {
 
 
 
-  
+  var swiper = new Swiper('.swiper-container', {
+    loop: true,
+  });
+
+  // $('.swiper__img').addClass('layer');
+  // $('.swiper__pict').parallax();
 });
